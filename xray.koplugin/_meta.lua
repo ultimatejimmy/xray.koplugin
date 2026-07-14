c71bdbc1cefbe3208c2695cb69dac0c624df2035
Jimmy Pautz <jpautz@watermarkinsights.com>
2026-07-14 09:28:46 -0500

    Release 26.7.14.1

--- xray.koplugin/_meta.lua
+++ xray.koplugin/_meta.lua
@@ -5,5 +5,5 @@ return {
     name = "xray",
     fullname = _("X-Ray"),
     description = _([[Transform your reading experience with AI-powered book analysis, just like Amazon Kindle X-Ray!]]),
-    version = "26.7.14"
+    version = "26.7.14.1"
 }
